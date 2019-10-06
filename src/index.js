45e3a2ace41bf284c85349a44c3b1a55e7dbc6ef
arturitov <aav13@my.fsu.edu>
2019-10-06 15:21:20 -0400

    Task 2: Add a Guarantee Section
    Added the section shown on the screenshot. Also added an event listener
    that performs an AJAX call to get the content of the modal when clicked.

--- src/index.js
+++ src/index.js
@@ -16,3 +16,19 @@ document.getElementById("play-button-overlay-zwflowymel")
           .querySelector('.custom-wistia-play-button').style.display = "none";
       }});
     });
+
+document.getElementById("gurantee-modal-link")
+  .addEventListener("click", function(){
+    var xhr = new XMLHttpRequest();
+    console.log("click")
+    xhr.open('GET', 'https://www.algaecal.com/wp-json/acf/v3/options/options');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const modalContent = JSON.parse(xhr.responseText)["acf"]["7yr_full_copy"];
+            document.getElementById('guarantee-modal-body').innerHTML = modalContent;
+              _wq.push({ id: 'sp-wistia-inc-oxo4ak3z2d', onReady: function(video) {
+              }});
+        }
+    };
+    xhr.send();
+  });

1ab3651386c026c23a62d0b70e4b06acd201c268
arturitov <aav13@my.fsu.edu>
2019-10-06 14:05:27 -0400

    Task 1: Click event needs to trigger the video to play
    When the image overlay wrapper is clicked then we get a handle on the
    wistia video and play it.

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ import 'bootstrap';
 document.addEventListener('DOMContentLoaded', () => {
   loadSVGs();
 });
+
+document.getElementById("play-button-overlay-zwflowymel")
+    .addEventListener("click", function(){
+      _wq.push({ id: 'sp-video_zwflowymel', onReady: function(video) {
+        video.play();
+        document
+          .querySelector('.custom-wistia-play-button').style.display = "none";
+      }});
+    });

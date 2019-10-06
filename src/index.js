a060f16c4b3c3dbb47b7db8343991be24393321a
arturitov <aav13@my.fsu.edu>
2019-10-06 17:11:23 -0400

    Fixed code style issues using npm run pretest which runs eslint --ignore-path .gitignore . --fix. I was able to fix all errors except 'no-underscore-dangle' but I was not able to get rid of that error because that is the variable set in the video player library.

--- src/index.js
+++ src/index.js
@@ -9,56 +9,53 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-var xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://www.algaecal.com/wp-json/acf/v3/options/options');
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        const acf = JSON.parse(xhr.responseText).acf;
+xhr.onload = function onOptionsLoad() {
+  if (xhr.status === 200) {
+    const { acf } = JSON.parse(xhr.responseText);
 
-        document.getElementById("play-button-overlay-zwflowymel")
-            .addEventListener("click", function(){
-              _wq.push({ id: 'sp-video_zwflowymel', onReady: function(video) {
-                video.play();
-                document
-                  .querySelector('.custom-wistia-play-button').style.display = "none";
-              }});
-            });
+    document.getElementById('play-button-overlay-zwflowymel')
+      .addEventListener('click', () => {
+        window._wq.push({
+          id: 'sp-video_zwflowymel',
+          onReady(video) {
+            video.play();
+            document
+              .querySelector('.custom-wistia-play-button').style.display = 'none';
+          },
+        });
+      });
 
-        document.getElementById("gurantee-modal-link")
-          .addEventListener("click", function(){
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'https://www.algaecal.com/wp-json/acf/v3/options/options');
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    const modalContent = acf["7yr_full_copy"];
-                    document.getElementById('guarantee-modal-body').innerHTML = modalContent;
-                      _wq.push({ id: 'sp-wistia-inc-oxo4ak3z2d', onReady: function(video) {
-                      }});
-                }
-            };
-            xhr.send();
-          });
+    document.getElementById('gurantee-modal-link')
+      .addEventListener('click', () => {
+        const modalContent = acf['7yr_full_copy'];
+        document.getElementById('guarantee-modal-body').innerHTML = modalContent;
+        window._wq.push({
+          id: 'sp-wistia-inc-oxo4ak3z2d',
+          onReady() {
+          },
+        });
+      });
 
 
-        const officeHours = acf.office_hours;
+    const officeHours = acf.office_hours;
 
-        var date = new Date();
-        const day = date.getDay();
-        const localeOptions = {
-          timeZone: "America/Los_Angeles",
-          hour12: false,
-          timeStyle: 'short'
-        }
-        const currentTime = date.toLocaleTimeString("en-US", localeOptions).split(":").join('');
-        const startingTime = officeHours[day].starting_time;
-        const closingTime =  officeHours[day].closing_time;
+    const date = new Date();
+    const day = date.getDay();
+    const localeOptions = {
+      timeZone: 'America/Los_Angeles',
+      hour12: false,
+      timeStyle: 'short',
+    };
+    const currentTime = date.toLocaleTimeString('en-US', localeOptions).split(':').join('');
+    const startingTime = officeHours[day].starting_time;
+    const closingTime = officeHours[day].closing_time;
 
-        if(currentTime < startingTime || currentTime > closingTime){
-            const removeElem = document.querySelector('.speak-to-our-bone-specialists');
-            removeElem.parentNode.removeChild(removeElem);
-          }
-
-        var usaTime = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
+    if (currentTime < startingTime || currentTime > closingTime) {
+      const removeElem = document.querySelector('.speak-to-our-bone-specialists');
+      removeElem.parentNode.removeChild(removeElem);
     }
+  }
 };
 xhr.send();
